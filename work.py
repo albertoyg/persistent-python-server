9998e005df1141a150445485eb012cd61f528e3b
Alberto Yanes <117393691+albertoyg@users.noreply.github.com>
2023-02-05 16:54:57 -0800

    Update work.py

--- work.py
+++ work.py
@@ -15,15 +15,11 @@ import os
 
 close = 0
 
-
 def checkForFile(requestline):
     decode = requestline.split()
     filename = decode[1][1:]
     return (os.path.isfile(filename), filename)
 
-def processRequest(request):
-    requests = request.split('\n')
-    checkMULTImessages(requests)
     # for request in requests:
     #     if re.search(re.compile(r"GET /.* HTTP/1.0"), request):
     #         if re.search(re.compile(r"connection:\s*Keep-alive", re.IGNORECASE), requests[+1]):
@@ -212,6 +208,9 @@ while inputs:
                                     if found: 
                                         HTMLfile = open(filename, 'r')
                                         ok = 'HTTP/1.0 200 OK\r\nConnection: keep-alive\r\n\r\n{content}'.format(content = HTMLfile.read())
+                                        sm = 'HTTP/1.0 200 OK'
+                                        log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = messages[curRequest], res = sm)
+                                        request_message[s].put(log)
                                     #   PRINT OK MESSAGE TO CLIENT 
                                         message_queues[s].put(ok)
                                     #   BUT FOR NOW....
@@ -229,7 +228,9 @@ while inputs:
                                     #   PRINT notfound MESSAGE TO CLIENT 
                                         message_queues[s].put(not_found)
                                     #   BUT FOR NOW....
-                                        print(not_found)
+                                        sm = 'HTTP/1.0 404 Not Found'
+                                        log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = messages[curRequest], res = sm)
+                                        request_message[s].put(log)
                                 else:
                                 # check for bad header line
                                     if not re.search(re.compile(r"connection:\s*close", re.IGNORECASE), messages[curRequest+1]) and messages[curRequest + 1] != '':
@@ -237,6 +238,9 @@ while inputs:
                                         bad_request = 'HTTP/1.0 400 Bad Request\r\n\r\n'
                                     #   PRINT notfound MESSAGE TO CLIENT 
                                         message_queues[s].put(bad_request)
+                                        sm = 'HTTP/1.0 400 Bad Request'
+                                        log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = messages[curRequest], res = sm)
+                                        request_message[s].put(log)
                                     #   BUT FOR NOW....
                                     found, filename = checkForFile(messages[curRequest])
                                     if found: 
@@ -245,13 +249,13 @@ while inputs:
                                     #   PRINT OK MESSAGE TO CLIENT 
                                         message_queues[s].put(ok)
                                     #   BUT FOR NOW....
-                                        print(ok)
+                                        sm = 'HTTP/1.0 200 OK'
+                                        log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = messages[curRequest], res = sm)
+                                        request_message[s].put(log)
                                     # output file contents 
                                         
                                     #   PRINT file contents TO CLIENT \c
-                                        message_queues[s].put(HTMLfile.read())
-                                    #   BUT for now....
-                                        print(HTMLfile.read())
+          
 
 
                                     else:
@@ -259,6 +263,9 @@ while inputs:
                                         not_found = 'HTTP/1.0 404 Not Found\r\nConnection: close\r\n\r\n'
                                         #   PRINT notfound MESSAGE TO CLIENT 
                                         message_queues[s].put(not_found)
+                                        sm = 'HTTP/1.0 404 Not Found'
+                                        log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = messages[curRequest], res = sm)
+                                        request_message[s].put(log)
                                         #   BUT FOR NOW....
                                         
             
@@ -271,14 +278,9 @@ while inputs:
                                         ok = 'HTTP/1.0 200 OK\r\n\r\n{content}'.format(content = HTMLfile.read())
                                     #   PRINT OK MESSAGE TO CLIENT 
                                         message_queues[s].put(ok)
-                                    #   BUT FOR NOW....
-                                        print(ok)
-                                    # output file contents 
-                                        
-                                    #   PRINT file contents TO CLIENT \c
-                                        message_queues[s].put(HTMLfile.read())
-                                    #   BUT for now....
-                                        print(HTMLfile.read())
+                                        sm = 'HTTP/1.0 200 OK'
+                                        log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = messages[curRequest], res = sm)
+                                        request_message[s].put(log)
 
                                     # check if end of requests: \n\n
                             elif messages[curRequest] == '':
@@ -296,7 +298,9 @@ while inputs:
         # ensure it's not the header
                                 if not re.search(re.compile(r"connection:\s*Keep-alive", re.IGNORECASE), messages[curRequest]) and not re.search(re.compile(r"connection:\s*close", re.IGNORECASE), messages[curRequest]) and messages[curRequest] != '':
                                     bad_request = 'HTTP/1.0 400 Bad Request\r\n\r\n'
-                                    print(bad_request)
+                                    sm = 'HTTP/1.0 400 Bad Request'
+                                    log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = messages[curRequest], res = sm)
+                                    request_message[s].put(log)
             #   PRINT notfound MESSAGE TO CLIENT 
                                     message_queues[s].put(bad_request)
             #   BUT FOR NOW....
@@ -313,6 +317,9 @@ while inputs:
                         bad_request = 'HTTP/1.0 400 Bad Request\r\n\r\n'
             #   PRINT notfound MESSAGE TO CLIENT 
                         message_queues[s].put(bad_request)
+                        sm = 'HTTP/1.0 400 Bad Request'
+                        log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = messages[curRequest], res = sm)
+                        request_message[s].put(log)
                         if s not in outputs:
                             outputs.append(s)
 
@@ -344,6 +351,9 @@ while inputs:
                                         not_found = 'HTTP/1.0 404 Not Found\r\nConnection: close\r\n\r\n'
                                             #   PRINT notfound MESSAGE TO CLIENT 
                                         message_queues[s].put(not_found)
+                                        sm = 'HTTP/1.0 404 Not Found'
+                                        log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = buffer[line][:-1], res = sm)
+                                        request_message[s].put(log)
                                             
                                     else:
                                         # found file and close
@@ -365,12 +375,19 @@ while inputs:
                                                 not_found = 'HTTP/1.0 404 Not Found\r\nConnection: keep-alive\r\n\r\n'
                                                 #   PRINT notfound MESSAGE TO CLIENT 
                                                 message_queues[s].put(not_found)
+                                                sm = 'HTTP/1.0 404 Not Found'
+                                                log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = buffer[line][:-1], res = sm)
+                                                request_message[s].put(log)
                                                 
                                             else:
                                             # found file and keep alive
                                                 HTMLfile = open(filename, 'r')
                                                 ok = 'HTTP/1.0 200 OK\r\nConnection: keep-alive\r\n\r\n{content}'.format(content = HTMLfile.read())
                                                 message_queues[s].put(ok)
+                                                sm = 'HTTP/1.0 200 OK'
+                                                log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = buffer[line][:-1], res = sm)
+                                                request_message[s].put(log)
+
                                                 
                                         else:
 
@@ -380,12 +397,18 @@ while inputs:
                                                 not_found = 'HTTP/1.0 404 Not Found\r\nConnection: close\r\n\r\n'
                                                 #   PRINT notfound MESSAGE TO CLIENT 
                                                 message_queues[s].put(not_found)
+                                                sm = 'HTTP/1.0 404 Not Found'
+                                                log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = buffer[line][:-1], res = sm)
+                                                request_message[s].put(log)
                                                 
                                             else:
                                             # found file and close
                                                 HTMLfile = open(filename, 'r')
                                                 ok = 'HTTP/1.0 200 OK\r\nConnection: close\r\n\r\n{content}'.format(content = HTMLfile.read())
                                                 message_queues[s].put(ok)
+                                                sm = 'HTTP/1.0 200 OK'
+                                                log = "{time}: {ipport} {req}; {res}".format(time = curtime, ipport = ipandport, req = buffer[line][:-1], res = sm)
+                                                request_message[s].put(log)
                                         
                                    #  process and close
 
@@ -449,4 +472,3 @@ while inputs:
     # if s not in readable and writable and exceptional:
     #     #handle timeout events
     #     socket.settimeout(30)
-
